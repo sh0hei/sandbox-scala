7f3316bf0ee96b7020ca8d795ecfb7ad86ebedb2
sh0hei <vividbit@gmail.com>
2015-07-20 05:04:28 +0900

    Initial commit.

---
+++ src/ex/basic/Hello.scala
@@ -0,0 +1,10 @@
+package ex.basic
+
+/**
+ * Created by sh0hei on 15/07/19.
+ */
+object Hello extends App {
+
+  println("Hello, Scala")
+
+}
---
+++ src/ex/bucket/Util.scala
@@ -0,0 +1,77 @@
+package ex.bucket
+
+import java.io.{File, FileWriter}
+
+import scala.reflect.api.TypeTags.TypeTag
+
+/**
+ * Created by sh0hei on 15/07/19.
+ */
+object Util {
+
+  def typeName[T: TypeTag](obj: T) = typeOf[T]
+
+  def stopWatchCore(fname: String, msec: Boolean, times: Int = 1)(body: => Unit) = {
+
+    val i = 0;
+    val start = System.nanoTime()
+    while (i < times) {
+      body
+      i += 1
+    }
+    val ns = System.nanoTime()
+    val s = timeReport((ns - start) / times, msec)
+    println(s)
+    if (fname != null) {
+      log(fname, s.replaceAll(" \\[", "\t[") + "\r\n")
+    }
+  }
+
+  def timeReport(ns: Long, useMsec: Boolean) = {
+    if (useMsec) {
+      "%7.3f [msec]".format(ns * 0.000001)
+    } else {
+      "%7.3f [usec]".format(ns * 0.001)
+    }
+  }
+  def log(fname: String, s: String): Unit = {
+    val fw: FileWriter = null
+    try {
+      val fw = new FileWriter(new File(fname), true)
+      fw.write(s)
+      fw.flush()
+    } catch {
+      case e: Exception => println("Error: " + e.getMessage())
+    } finally {
+      if (fw != null) fw.close()
+    }
+  }
+
+  def stopWatch(t: Int = 1) = stopWatchCore(null, false, t)_
+
+  def stopWatchMsec(t: Int = 1) = stopWatchCore(null, true, t)_
+
+  def stopWatchToLog(f: String, t: Int = 1) = stopWatchCore(f, false, t)_
+
+  def stopWatchMsecToLog(f: String, t: Int = 1) = stopWatchCore(f, true, t)_
+
+  var level = 0
+  var indicator = "- "
+
+  def trace[T](fname: String, arg: Any*)(body: => T): T = {
+    println((indicator * level) + fname + " (" + arg.mkString(",") + ")")
+    level += 1
+    val ret = body
+    level -= 1
+    println((indicator * level) + fname + "= " + ret)
+    ret
+  }
+
+  def loop(n: Int)(bodyFun: (Int) => Unit): Unit = {
+    var i = 0
+    while (i < n) {
+      bodyFun(i)
+      i += 1
+    }
+  }
+}
