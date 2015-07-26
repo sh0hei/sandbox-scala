33757658a7f3bf2dfd0786807217bd3ff5ddb252
sh0hei <vividbit@gmail.com>
2015-07-27 00:00:02 +0900

    Add Literal.scala

---
+++ src/ex/basic/Literal.scala
@@ -0,0 +1,14 @@
+package ex.basic
+
+/**
+ * Created by sh0hei on 15/07/26.
+ */
+object Literal extends App {
+
+  val num1 = 123
+  val num2 = 123.4
+
+  def test1(): Unit = {
+    println("\n== test Types & Vlues of Literals ===\n")
+  }
+}
